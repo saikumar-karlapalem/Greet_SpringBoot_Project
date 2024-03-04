272ef2dac78be3919710ac0e6227cf87a4d87a51
saikumar-karlapalem <karlapalemsaikumar92@gmail.com>
2024-03-04 08:46:37 +0530

    first commit

---
+++ src/main/java/com/saiko/sample/one/controller/SaikoDemoController.java
@@ -0,0 +1,18 @@
+package com.saiko.sample.one.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saiko/")
+public class SaikoDemoController {
+	
+	@GetMapping("/{message}")
+	public String greet(@PathVariable("message") String message)
+	{
+		return "hello"+message;
+	}
+
+}
